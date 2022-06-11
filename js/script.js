dda7e6a64ee87030ce8a7da617d3e72cb8d6ade2
Nicole <nicole.louise.braz@gmail.com>
2022-06-11 12:50:00 -0300

    projeto js do contador

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber=0
+var i = currentNumber;
+
+function add(){
+    currentNumberWrapper.innerHTML = currentNumber + 1;
+    currentNumber=currentNumber+1;
+    console.log('currentNumer');
+}
+
+function decrement(){ 
+    currentNumberWrapper.innerHTML = currentNumber - 1;
+    currentNumber=currentNumber-1;
+    console.log('currentNumer');
+    
+}
+
